baf12183bc8bd3fee2d949c6d0098d03762143f6
Igor Yagushevsky <yagushevskij@gmail.com>
2020-09-27 18:35:46 +0300

    switched off validation messages in console

--- middlewares/errHandler.js
+++ middlewares/errHandler.js
@@ -4,5 +4,5 @@ module.exports = (err, req, res, next) => {
   const status = err.statusCode || 500;
   const { message } = err;
   res.status(status).json({ message: message || errMessages.serverError });
-  next(err);
+  next();
 };
